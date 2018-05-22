7e4c13568efaf94367dd1f5747eb141e68fa31dd
tian1314 <1558606166@qq.com>
2018-05-22 17:11:43 +0800

    version 2

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -23,4 +23,44 @@ class UserController extends Yaf_Controller_Abstract
         echo BladeView::view('user.test',['name'=>'john1']);
 
     }
+
+    /**
+     * curl
+     */
+    public function curlAction(){
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+        dd($res->getStatusCode());
+    }
+
+    /**
+     * redis
+     */
+    public function redisAction(){
+        $config = Yaf_Application::app()->getConfig()->redis->toArray();
+        $client = new \Predis\Client($config);
+        $client->set('foo', 'bar');
+        $value = $client->get('foo');
+        dd($value);
+    }
+
+    public function ptalkAction(){
+        $pheanstalk = new \Pheanstalk\Pheanstalk('127.0.0.1');
+        $pheanstalk
+            ->useTube('testtube')
+            ->put("job payload goes here\n");
+
+// ----------------------------------------
+// worker (performs jobs)
+
+        $job = $pheanstalk
+            ->watch('testtube')
+            ->ignore('default')
+            ->reserve();
+
+        echo $job->getData();
+        //$pheanstalk->delete($job);
+        $pheanstalk->getConnection()->isServiceListening();
+        die;
+    }
 }
